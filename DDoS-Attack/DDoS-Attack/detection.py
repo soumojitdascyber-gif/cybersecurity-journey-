feee7ae920cbe04042a0e69b4742ec5bbfb9afac
SOUMOJIT DAS <soumojit.das.cyber@gmail.com>
2026-04-15 23:29:50 +0530

    Delete detection.py

--- DDoS-Attack/DDoS-Attack/detection.py
+++
@@ -1,9 +0,0 @@
-def detect_ddos(requests):
-    threshold = 50
-
-    if requests > threshold:
-        print("⚠️ Possible DDoS Attack Detected!")
-    else:
-        print("Normal Traffic")
-
-detect_ddos(80)
